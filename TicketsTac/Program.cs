861f0270e39939048c213dc511054845ba8e5079
Zozoleclown <Zozoleclown1@gmail.com>
2016-03-11 11:53:44 +0100

    Finalisation de DB et début de InterfaceManager

--- TicketsTac/Program.cs
+++ TicketsTac/Program.cs
@@ -6,12 +6,10 @@ namespace TicketsTac
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("coucou");
-            // DB.Migrate();                // Décommenter pour que l'exécution prépare la BDD
+            //DB.Migrate();                // Décommenter pour que l'exécution prépare la BDD
 
             /* Tests Pierrick */
-            User u = new User(0, "Zozo", "Zozoleclown1@gmail.com", Rank.Administrateur);
-            DB.Insert<User>(u, "users");
+            InterfaceManager im = new InterfaceManager();
             // Fin tests Pierrick */
 
             Console.ReadLine();
