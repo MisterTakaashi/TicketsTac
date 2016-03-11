306666a6ebef97b3eb31f1195f50dc3e5de4bf5d
Zozoleclown <Zozoleclown1@gmail.com>
2016-03-11 10:54:33 +0100

    Suppression de tests dans Program.cs

--- TicketsTac/Program.cs
+++ TicketsTac/Program.cs
@@ -10,9 +10,6 @@ namespace TicketsTac
             // DB.Migrate();                // Décommenter pour que l'exécution prépare la BDD
 
             /* Tests Pierrick */
-            int buffer = 0;
-            Console.WriteLine(int.TryParse("Z", out buffer).ToString());
-            /*
             User u = new User(0, "Zozo", "Zozoleclown1@gmail.com", Rank.Administrateur);
             DB.Insert<User>(u, "users");
             // Fin tests Pierrick */
