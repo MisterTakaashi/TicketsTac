f92bb2b8f1977f4a39500bc4ec0c0c144bdff08f
Mat-kun <mathieu.giorgiutti@y-nov.com>
2016-03-16 13:58:33 +0100

    Creation page New Ticket

---
+++ TicketsTacGui/NewTicketPage..xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicketsTacGui
+{
+    /// <summary>
+    /// Logique d'interaction pour NewTicketPage.xaml
+    /// </summary>
+    public partial class NewTicketPage : Page
+    {
+        private Projet Project { get; set; }
+        public NewTicketPage(int id)
+        {
+            this.Project = Projet.GetProjetFromBDD(id);
+            InitializeComponent();
+
+            List<User> users = User.GetAll();
+            foreach (User user in users)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Tag = user.Id;
+                item.Content = user.Username;
+
+                comboBox_assignee.Items.Add(item);
+            }
+        }
+
+        private void textBox_ticket_name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void buttonCreateTicket_Click(object sender, RoutedEventArgs e)
+        {
+            User u = new User(DB.Get(int.Parse(((ComboBoxItem)comboBox_assignee.SelectedItem).Tag.ToString()), "Users"));
+            Ticket created = new Ticket(textBox_ticket_name.Text, u, User.currentUser);
+            NavigationService.Navigate(new ProjectsListPage());
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ProjectsListPage());
+        }
+    }
+}
