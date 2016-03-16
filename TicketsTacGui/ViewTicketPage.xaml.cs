9b3c5f3cb3eddc0040396b309b968b7da7fcce97
Zozoleclown <Zozoleclown1@gmail.com>
2016-03-16 19:34:58 +0100

    Revue de la presentation de ViewTicketPage et corrections dans DB.cs

--- TicketsTacGui/ViewTicketPage.xaml.cs
+++ TicketsTacGui/ViewTicketPage.xaml.cs
@@ -35,18 +35,30 @@ namespace TicketsTacGui
                 TextBlock reply = new TextBlock();
                 reply.Background = Brushes.AntiqueWhite;
 
-                //replies.Children.Add(reply);
+                replies.Children.Add(reply);
             }
         }
 
         private void buttonCancel_Click(object sender, RoutedEventArgs e)
         {
-
+            NavigationService.Navigate(new ProjectIssuesPage(Ticket.Project.Id));
         }
 
         private void buttonSubmit_Click(object sender, RoutedEventArgs e)
         {
+            List<string> fields = new List<string>();
+            List<string> values = new List<string>();
 
+            fields.Add("Ticket_Id");
+            fields.Add("Message");
+            fields.Add("Created");
+
+            values.Add(Ticket.Id.ToString());
+            values.Add("'" + textBox.Text + "'");
+            values.Add(DB.getTimestamp().ToString());
+
+            DB.Insert(fields, values, "Ticket_comms");
+            NavigationService.Navigate(new ProjectIssuesPage(Ticket.Project.Id));
         }
     }
 }
