b2b77e1c2ab056e1d3277adb5d61c8adb241d26e
Zozoleclown <Zozoleclown1@gmail.com>
2016-03-17 00:42:09 +0100

    Correction d'une erreur de syntaxe dans Ticket_comms.sql

--- SQL/dbo.Ticket_comms.sql
+++ SQL/dbo.Ticket_comms.sql
@@ -5,7 +5,7 @@
     [Created]   BIGINT NOT NULL,
     [Creator_Id] INT   NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Ticket_comms_Tickets] FOREIGN KEY ([Ticket_Id]) REFERENCES [dbo].[Tickets] ([Id])
+    CONSTRAINT [FK_Ticket_comms_Tickets] FOREIGN KEY ([Ticket_Id]) REFERENCES [dbo].[Tickets] ([Id]),
     CONSTRAINT [FK_Ticket_comms_User] FOREIGN KEY ([Creator_Id]) REFERENCES [dbo].[Users] ([Id])
     );
 
